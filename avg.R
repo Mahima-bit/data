a013cc07ccb9bdc7d0ae98e797b5b4d232d055ea
Mahima-bit <60845389+Mahima-bit@users.noreply.github.com>
2020-03-10 11:40:57 +0530

    4

---
+++ avg.R
@@ -0,0 +1,3 @@
+IntervalDT <- activityDT[, c(lapply(.SD, mean, na.rm = TRUE)), .SDcols = c("steps"), by = .(interval)] 
+
+ggplot(IntervalDT, aes(x = interval , y = steps)) + geom_line(color="blue", size=1) + labs(title = "Avg. Daily Steps", x = "Interval", y = "Avg. Steps per day")
